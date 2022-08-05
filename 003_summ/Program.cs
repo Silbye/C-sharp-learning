9da386950548e61f4b1097dca7c944225cbb6d65
Silbye <silverxlxl9@gmail.com>
2022-08-05 04:58:25 +0300

    added random numbers in summ and added if else programm

--- 003_summ/Program.cs
+++ 003_summ/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 1;
-int numberB = 8;
+﻿int numberA = new Random().Next(1, 15);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(2, 20);
+Console.WriteLine(numberB);
 int summ = numberA + numberB;
 Console.WriteLine(summ);
---
+++ 005_ifelse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "silbye")
+{
+    Console.WriteLine("This username is already taken");
+}
+else
+{
+    Console.WriteLine("Hello, " + username + ".");
+}
